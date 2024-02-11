ea858dbb85d65e8edc475cb7cdc0c2ceb552b5d6
Hansadhwaja Biswal <hansadhwajabiswal@gmail.com>
2024-02-11 23:31:22 +0530

    update update-prompt folder

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,7 +2,7 @@
 
 import Form from "@components/Form"
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 const EditPrompt = () => {
     const router = useRouter();
@@ -55,13 +55,16 @@ const EditPrompt = () => {
 
     }
     return (
-        <Form
+        <Suspense>
+            <Form
             type="Edit"
             post={post}
             setPost={setPost}
             submitting={submitting}
             handleSubmit={updatePrompt}
         />
+        </Suspense>
+       
     )
 }
 
