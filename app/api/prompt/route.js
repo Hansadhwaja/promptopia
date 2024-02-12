5870a52b0d7bf2cca9818f2e82abac918a45f98f
Hansadhwaja Biswal <hansadhwajabiswal@gmail.com>
2024-02-12 15:29:02 +0530

    initial commit

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,8 +4,13 @@ import {  connectToDB } from "@utils/database";
 export const GET = async () => {
     try {
         await connectToDB();
-
-        const prompts = await Prompt.find({}).populate('creator');
+        const prompts = await Prompt.find({}, (err, users) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(users);
+            }
+        }).populate('creator');
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
